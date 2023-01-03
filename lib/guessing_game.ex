1cd45890e235bde35a08334fd129ddee86d33887
Ayiko Fred <lgmfred@ayikoyo.com>
2023-01-03 13:43:07 +0300

    finish basic guess game

---
+++ lib/guessing_game.ex
@@ -0,0 +1,17 @@
+defmodule Games.GuessingGame do
+  def play do
+    number = Enum.random(1..10)
+    guess = guess()
+    if number == guess do
+      "Correct!"
+    else
+      "Incorrect!"
+    end
+  end
+
+  defp guess do
+    IO.gets("Enter your guess: ")
+    |> String.trim()
+    |> String.to_integer()
+  end
+end
